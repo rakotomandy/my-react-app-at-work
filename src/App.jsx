f99d164990cbd218e23f8b284032013b659516a6
rakotomandy <ericrakotomandimby@gmail.com>
2025-11-07 19:25:01 +0300

    My-react-app at work

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+        <h1 className='underline text-lg text-blue-300 font-bold text-center'>HELLO WORLD</h1>
+    </>
+  )
+}
+
+export default App
